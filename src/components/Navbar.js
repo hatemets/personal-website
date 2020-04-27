5b065ceec44a2921500f33acbc1276715c5ca975
hatemets <mistermark009@gmail.com>
2020-04-27 23:01:26 +0300

    Improve navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <nav>
             <div className="wrapper">
-                Mark Heidmets
+                <Link to='/'>MARK</Link>
+                <ul id="navlist">
+                    <li>Home</li>
+                    <li>About Me</li>
+                    <li>Projects</li>
+                </ul>
             </div>
         </nav>
     )
