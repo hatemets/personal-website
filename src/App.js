af9fa4505a9827f28cb4f22ff810f4e05af04ebf
hatemets <mistermark009@gmail.com>
2020-05-08 21:51:03 +0300

    Improve navigation bar transitioning and layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
 import { Projects } from './components/Projects';
 import { About } from './components/About';
 import { AnimatePresence } from 'framer-motion';
 import { Route, Switch, useLocation } from 'react-router-dom';
+import variables from './styles/_base.scss';
 
-const App = _ => {
+const App = () => {
 	const location = useLocation();
+	const navigationBarHeight = Number(variables.navbarHeight.slice(0, -2));
+	const color = variables.mainColor;
 	
+	useEffect(_ => {
+		let navigationBar = document.querySelector('nav');
+
+		document.addEventListener('scroll', () => {
+			let content = document.querySelector('.card');
+			let rect = content.getBoundingClientRect();
+			navigationBar.style.background = rect.y < navigationBarHeight ? color : color + "bb";
+		});
+	});
+
 	return (
 		<div id="App">
 			<Navbar />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { pageAnimation, popupAnimation } from '../assets/animationProperties';
 
 export const Home = () => {
 	const { variants: popupVariants, transition: popupTransition } = popupAnimation;
 	const { variants: pageVariants, transition: pageTransition } = pageAnimation;
-	
+
     return (
-		<div className="component-wrapper">
-			<motion.div id="image-one" className="image">
+		<motion.div className="component-wrapper">
+			<div id="image-one" className="image">
 				<motion.div
 					initial="initial"
 					animate="in"
@@ -27,29 +27,27 @@ export const Home = () => {
 							</div>
 							<div className="card">
 								<h1>Thank you for visiting my website!</h1>
-								<p>Here you can find out more about who I am and what I do. In addition, you can also take a look at some of the projects that I've been working on.</p>
+								<p>Here you can find out more about who I am and what I do. In addition, you can also take a look at some of the projects I've been working on.</p>
 							</div>
 				</motion.div>
-			</motion.div>
+			</div>
 
 			<section id="section-one">
-				<h1 className="s1">Who am I?</h1>
-				<p className="s2">Nostrud enim id sunt amet laborum nulla.qui cillum minim mollit qui eu. Irure ut voluptate culpa est officia deserunt cupidatat sit velit. Irure sint sit labore do aliqua ut velit veniam ut tempor non deserunt sint.</p>
+				<h1>Who am I?</h1>
+				<p>Who am I, anyway? My name is Mark Heidmets and I'm currently
+						studying at Gustav Adolf Grammar School in Tallinn, Estonia.
+						I have been programming for approximately two years, focusing
+						mainly on web design and browser automation.</p>
 				<div className="s3"></div>
 			</section>
 
-			<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			id="image-two"
-			className="image"></motion.div>
+			<div id="image-two" className="image"></div>
 
 			<section id="section-two">
 				<h1>Some Cool Text</h1>
 				<p>Irure nisi esse aliqua dolore culpa esse non incididunt sunt ipsum. Sit eiusmod minim do nisi ut occaecat mollit sint amet ea. Do aliqua reprehenderit cupidatat ullamco ipsum. Id anim ipsum voluptate ipsum proident cupidatat dolor do ullamco. Nisi commodo deserunt Lorem Lorem magna esse nostrud irure.</p>
 			</section>
 
-		</div>
+		</motion.div>
     )
 }
