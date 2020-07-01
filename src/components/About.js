49e31fcfb5492039f59a1b64771e3854acd41d96
hatemets <mistermark009@gmail.com>
2020-07-01 22:28:30 +0300

    Add personal bests section to about page

--- src/components/About.js
+++ src/components/About.js
@@ -42,6 +42,29 @@ export const About = () => {
 						relations or making me more resilient. I look forward to continuing with track in
 						college as well for it is an activity that I've been doing for over 7 years by now.
 					</p>
+					<h3 id="t3">Personal bests</h3>
+					<table id="tb">
+						<tr>
+							<th>Event</th>
+							<th>Time</th>
+						</tr>
+						<tr>
+							<td>60m</td>
+							<td>7.07</td>
+						</tr>
+						<tr>
+							<td>100m</td>
+							<td>11.18</td>
+						</tr>
+						<tr>
+							<td>200m</td>
+							<td>22.8</td>
+						</tr>
+						<tr>
+							<td>400m</td>
+							<td>50.59</td>
+						</tr>
+					</table>
 				</div>
 			</motion.div>
 		</div>
