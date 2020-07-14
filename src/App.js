f6e3393d07415b640a477195b3146359052e0814
hatemets <mistermark009@gmail.com>
2020-07-14 22:41:43 +0300

    Remove warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import { About } from "./components/About";
